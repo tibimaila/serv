0703207f32ae2d4fa597d8c83b1c3ee48228068a
tibimaila <tibimakhura@gmail.com>
2026-09-20 21:54:40 +0200

    test: add ServProperties configuration test

--- src/test/java/io/serv/config/ServPropertiesTest.java
+++ src/test/java/io/serv/config/ServPropertiesTest.java
@@ -22,7 +22,7 @@ public class ServPropertiesTest {
             );
 
     @Test
-    void testPropertiesBinding() {
+    void testDefaultProperties() {
         contextRunner.run(context -> { ServProperties properties = context.getBean(ServProperties.class);
             assertThat(properties.DataDir()).isEqualTo("/tmp/data");
             assertThat(properties.StorageRoot()).isEqualTo("/tmp/drive-data");
@@ -35,6 +35,31 @@ public class ServPropertiesTest {
             assertThat(properties.CorsOrigin()).isEqualTo("http://localhost:5173");
         });
     }
+
+    @Test
+    void testPropertiesBinding() {
+        contextRunner.withPropertyValues(
+                "serv.data-dir=./data",
+                "serv.storage-root=./drive-data",
+                "serv.presign-ttl-seconds=1800",
+                "serv.multipart-threshold=209715200",
+                "serv.part-size=20971520",
+                "serv.max-drive-size=2000000000",
+                "serv.root-email=user@serv.local",
+                "serv.root-password=user"
+        ).run(context -> {
+            ServProperties properties = context.getBean(ServProperties.class);
+            assertThat(properties.DataDir()).isEqualTo("./data");
+            assertThat(properties.StorageRoot()).isEqualTo("./drive-data");
+            assertThat(properties.PresignTtlSeconds()).isEqualTo(1800);
+            assertThat(properties.MultipartThreshold()).isEqualTo(209715200);
+            assertThat(properties.PartSize()).isEqualTo(20971520);
+            assertThat(properties.MaxDriveSize()).isEqualTo(2000000000);
+            assertThat(properties.RootEmail()).isEqualTo("user@serv.local");
+            assertThat(properties.RootPassword()).isEqualTo("user");
+        });
+    }
+
     @EnableConfigurationProperties(ServProperties.class)
     static class TestConfig {}
 }
