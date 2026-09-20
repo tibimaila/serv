d25bfed1b53c50513e168598e17bd94c4fc9fc9c
tibimaila <tibimakhura@gmail.com>
2026-09-20 19:19:13 +0200

    fix: bootstrap Spring application

--- src/main/java/io/serv/Application.java
+++ src/main/java/io/serv/Application.java
@@ -1,9 +1,13 @@
 package io.serv;
 
+
+import io.serv.config.Properties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(Properties.class)
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
