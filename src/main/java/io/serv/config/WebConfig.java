a59fb949721111250599a3b98d236db098aa3a15
tibimaila <tibimakhura@gmail.com>
2026-09-20 19:38:29 +0200

    feat: configure web MVC settings

---
+++ src/main/java/io/serv/config/WebConfig.java
@@ -0,0 +1,21 @@
+package io.serv.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    private final Properties properties;
+
+    public WebConfig(Properties properties) { this.properties = properties; }
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+                .allowedHeaders("*");
+    }
+
+}
