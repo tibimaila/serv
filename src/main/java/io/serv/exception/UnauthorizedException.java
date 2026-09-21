6f9181e1d8109311bb638cba767c853e622c947e
tibimaila <tibimakhura@gmail.com>
2026-09-22 00:56:08 +0200

    feat: add unauthorized exception

---
+++ src/main/java/io/serv/exception/UnauthorizedException.java
@@ -0,0 +1,9 @@
+package io.serv.exception;
+
+import org.springframework.http.HttpStatus;
+
+public class UnauthorizedException extends ApiException {
+    public UnauthorizedException(String message) {
+        super(HttpStatus.UNAUTHORIZED, "UNAUTHORIZED", message);
+    }
+}
