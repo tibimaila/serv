d8eff7d58086be44397e6d2d7af6cdbd29c1417f
tibimaila <tibimakhura@gmail.com>
2026-09-21 22:31:00 +0200

    refactor: rename application class

--- src/main/java/io/serv/ServApplication.java
+++ src/main/java/io/serv/ServApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 
 @SpringBootApplication
 @EnableConfigurationProperties(ServProperties.class)
-public class Application {
+public class ServApplication {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
