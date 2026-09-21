b8c61f185f285b66ee344e3f1cb5780404106ed3
tibimaila <tibimakhura@gmail.com>
2026-09-21 10:58:34 +0200

    fix: import Optional in FolderStore

--- src/main/java/io/serv/data/FolderStore.java
+++ src/main/java/io/serv/data/FolderStore.java
@@ -2,6 +2,10 @@ package io.serv.data;
 
 import io.serv.domain.Folder;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
 public interface FolderStore extends Store<Folder, UUID> {
 
     List<Folder> findByDriveId(UUID driveId);
