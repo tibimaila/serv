47c2f63bb9447d5354c45f172be31be1d4b339d3
tibimaila <tibimakhura@gmail.com>
2026-09-21 10:56:17 +0200

    fix: add imports for store interfaces

--- src/main/java/io/serv/data/ShareStore.java
+++ src/main/java/io/serv/data/ShareStore.java
@@ -3,6 +3,10 @@ package io.serv.data;
 import io.serv.data.Store; 
 import io.serv.domain.ShareLink;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
 public interface ShareStore extends Store<ShareLink, UUID> {
     
     Optional<ShareLink> findByToken(String token);
