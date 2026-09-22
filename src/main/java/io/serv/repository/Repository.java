944dea90a1a790c1660ec40dcbf930c330a1398f
tibimaila <tibimakhura@gmail.com>
2026-09-22 14:59:26 +0200

    refactor: rename Store to Repository

---
+++ src/main/java/io/serv/repository/Repository.java
@@ -0,0 +1,15 @@
+package io.serv.repository;
+
+import java.util.List; 
+import java.util.Optional;
+
+public interface Store<T, ID> {
+    
+    Optional<T> findById(ID id);
+
+    List<T> findAll();
+
+    T save(T entity);
+
+    void deleteById(ID id);
+}
