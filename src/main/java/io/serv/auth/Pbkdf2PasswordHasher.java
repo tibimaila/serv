cc1c0fa206549ce69cde0675fd7e983bc2753c25
tibimaila <tibimakhura@gmail.com>
2026-09-22 02:27:08 +0200

    feat: implement password hashing with PBKDF2

---
+++ src/main/java/io/serv/auth/Pbkdf2PasswordHasher.java
@@ -0,0 +1,27 @@
+package io.serv.auth;
+
+public class Pbkdf2PasswordHasher implements PasswordHasher {
+
+    private static final int SALT_LENGTH_BYTES = 16;
+    private static final int HASH_WIDTH_BITS = 256;
+
+    private final Pbkdf2PasswordEncoder encoder;
+
+    public Pbkdf2PasswordHasher(ServProperties props) {
+        this.encoder = new Pbkdf2PasswordEncoder(
+                SALT_LENGTH_BYTES,
+                props.security().pbkdf2Iterations(),
+                HASH_WIDTH_BITS
+        );
+
+        this.encoder.setEncodeHashAsBase64(true);
+    }
+
+    @Override
+    public String hash(String password) { return encoder.encode(password); }
+
+    @Override
+    public boolean matches(String password, String hashedPassword) {
+        return encoder.matches(password, hashedPassword);
+    }
+}
